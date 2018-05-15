7227779ed48d53ada4cf7da854c61b2b8fd338e7
idali0226 <ida.li@nrm.se>
2018-05-15 14:19:41 +0200

    Remove body-parser from index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 
 const app = express()
-app.use(bodyParser.json())
 
 const todos = require('./src/routes/todos')
 const users = require('./src/routes/users')
