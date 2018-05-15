e8a6244026b6eaf044ed90d0dc6183478bc16fc1
idali0226 <ida.li@nrm.se>
2018-05-15 14:26:10 +0200

    Sequelize user table with postgres

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { User } = require('../sequelize')
+const { User, Todo } = require('../sequelize')
 
 const router = express.Router()
 const jsonParser = bodyParser.json()
@@ -8,13 +8,55 @@ const jsonParser = bodyParser.json()
 router
   .route('/')
   .get((req, res) => {
-    console.log('get users')
-    User.findAll().then(users => res.json(users))
+    User.findAll({
+      include: [
+        {
+          model: Todo,
+          as: 'todos',
+        },
+      ],
+    }).then(users => res.json(users))
   })
   .post(jsonParser, (req, res) => {
     const body = req.body
 
     User.create(body).then(user => res.json(user))
+  })
+
+router.route('/search').get((req, res) => {
+  const { name } = req.query
+
+  if (name) {
+    query = User.findAll({
+      include: [
+        {
+          model: Todo,
+          as: 'todos',
+        },
+      ],
+      where: { name: name },
+    })
+
+    query.then(users => res.json(users))
+  }
+})
+
+router
+  .route('/:id')
+  .all((req, res, next) => {
+    const { id } = req.params
+    User.findById(id).then(usr => {
+      req.user = usr
+      next()
+    })
+  })
+  .get((req, res) => {
+    const user = req.user
+    if (!user) {
+      res.sendStatus(404)
+    } else {
+      res.status(200).json(user)
+    }
   })
 
 module.exports = router
